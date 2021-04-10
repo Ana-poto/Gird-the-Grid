2adc2a630c9439805ed9956ea74433bd71a9f180
ancestor-mithril <sgeorge.sstoica99@gmail.com>
2021-04-10 21:41:29 +0300

    Finished testing user confirmation

--- BlazorServer/Models/ConfirmRegistrationMailRequest.cs
+++ BlazorServer/Models/ConfirmRegistrationMailRequest.cs
@@ -15,10 +15,10 @@ namespace BlazorServerAPI.Models
         public static string ConfirmRegistrationBodyBuilder(string userId)
         {
             //TODO: @first: check if this is illegal (see what SQ says). Olariu said no static methods in nonstatic classes. As for me, I like it.
-            return $"Thanks for signing up with Gird-the-Grid!\n\n" +
-                $"You must follow this link to activate your account: http://localhost:49429/auth/confirm/?id={userId} \n\n" +
-                $"Have fun, and don't hesitate to contact us with your feedback.\n" +
-                $"Gird-the-Grid team" +
+            return $"Thanks for signing up with Gird-the-Grid!\r\n\r\n" +
+                $"You must follow this link to activate your account: http://localhost:49429/auth/confirm?userId={userId} \r\n\r\n" +
+                $"Have fun, and don't hesitate to contact us with your feedback.\r\n" +
+                $"Gird-the-Grid team, \r\n" +
                 $"http://localhost:49481/";
             //TODO: When no other task exists, maybe add css to mail
         }
